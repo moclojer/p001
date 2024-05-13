a346ea2e181a8b9020b089b03aa76df13a410a09
Avelino <31996+avelino@users.noreply.github.com>
2024-05-13 06:51:55 -0300

    squid port dynamic, read envvar
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 htpasswd -b -c /apps/htpasswd $P001_USER $P001_PASS
+sed "s/P001_PORT/$P001_PORT/g" /apps/squid.conf.template > /apps/squid.conf
 /apps/squid/sbin/squid -f /apps/squid.conf -NYCd 1
