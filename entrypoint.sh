c7d49231dae87b71ce197a5485601c6c7f00d6fe
Josué Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-05-16 10:55:41 -0300

    style: quote vars to prevent globbing and word splitting
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 source "/src/.env"
 
-htpasswd -c -B -b /etc/squid/htpasswd ${P001_USER} ${P001_PASS}
+htpasswd -c -B -b /etc/squid/htpasswd "${P001_USER}" "${P001_PASS}"
 squid -f /etc/squid/squid.conf -NYCd 1
