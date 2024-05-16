096b6253231262e640d019db35028ed1d3228544
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-05-16 10:50:07 -0300

    fix: load and expose environment on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-htpasswd -c -B -b /etc/squid/htpasswd "$P001_USER" "$P001_PASS"
+source /src/.env
+
+htpasswd -c -B -b /etc/squid/htpasswd ${P001_USER} ${P001_PASS}
 squid -f /etc/squid/squid.conf -NYCd 1
